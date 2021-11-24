c04b1aa4b5fd3d9e9961abecbab08b47fd8e809d
Damien Clauzon <damien.clauzon@grenoble-inp.org>
2021-11-24 11:15:38 +0100

    Test offers OK

--- code/src/Offer.java
+++ code/src/Offer.java
@@ -1,6 +1,9 @@
 import java.sql.Date;
+import java.sql.Time;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.math.*;
+import java.nio.channels.spi.SelectorProvider;
 
 public class Offer {
 	private float newPrice;
@@ -14,6 +17,16 @@ public class Offer {
 		this.idProduit = idProduct;
 		this.idCompte = idCompte;
 		java.util.Date currentDate = new java.util.Date();
+		this.date = new java.sql.Date(currentDate.getTime());
+	}
+
+	public Offer(float newPrice, int idProduct, int idCompte, int delay) {
+		this(newPrice, idProduct, idCompte);
+		java.util.Date currentDate = new java.util.Date();
+		Calendar c = Calendar.getInstance();
+		c.setTime(currentDate);
+		c.add(Calendar.SECOND, delay);
+		currentDate = c.getTime();
 		this.date = new java.sql.Date(currentDate.getTime());
 	}
 
@@ -49,13 +62,20 @@ public class Offer {
 	public static void addTestOffers(Database database) {
 		System.out.println("Remplissage des offres\n...");
 		Iterator<ArrayList<Float>> iterator = database.jsonParse.parseFloatArray("fakeOffers");
+		int oldIdProduct = -1;
+		int delay = 0;
 		while (iterator.hasNext()) {
 			ArrayList<Float> aList = iterator.next();
 			Iterator<Float> it = aList.iterator();
 			float newPrice = ((Number) it.next()).floatValue();
 			int idProduct = Math.round(((Number) it.next()).floatValue());
+			try {
+				Thread.sleep(1);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			int idCompte = Math.round(((Number) it.next()).floatValue());
-            (new Offer(newPrice, idProduct, idCompte)).insertOffre(database);
+            (new Offer(newPrice, idProduct, idCompte, delay++)).insertOffre(database);
         }
 		System.out.println("Remplissage des offres terminé");
 	}
